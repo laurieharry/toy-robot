d2236aa3c43817eadd2457cdf2dc9ec797c6d588
Laurence Harrison <laurieharry@gmail.com>
2019-06-20 16:56:08 +1000

    add toyRobot.js and begin creating Robot and Table classes

---
+++ toyRobot.js
@@ -0,0 +1,24 @@
+class Robot {
+  constructor(facing, xCoord, yCoord) {
+    this.facing = facing;
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  }
+
+  //PLACE X,Y,F
+
+  //MOVE
+
+  //LEFT
+
+  //RIGHT
+
+  //REPORT
+}
+
+class Table {
+  constructor(xMax, yMax) {
+    this.xMax = xMax;
+    this.yMax = yMax;
+  }
+}
